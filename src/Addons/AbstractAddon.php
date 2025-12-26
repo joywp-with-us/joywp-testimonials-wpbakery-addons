d55b0ad0628bdcf1f208d0cd49e726f91565203f
DanilaStrogov <admin@joywp.com>
2025-12-26 10:38:43 +0200

    Introduce addon abstract class

---
+++ src/Addons/AbstractAddon.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * Abstract addon class.
+ * Functionality that common for all addons builders.
+ *
+ * @since 1.0
+ */
+
+namespace JoywpTestimonialsWpb\Addons;
+
+use JoywpTestimonialsWpb\Addons\Builders\Wpbakery\Addon\Addon;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Abstract addon class.
+ *
+ * @since 1.0
+ */
+abstract class AbstractAddon {
+	/**
+	 * Addon slug.
+	 *
+	 * @since 1.0
+	 */
+	public string $addon_slug;
+
+	/**
+	 * Addon template file.
+	 *
+	 * @since 1.0
+	 */
+	public string $template;
+
+	/**
+	 * Addon config.
+	 *
+	 * @since 1.0
+	 */
+	public array $config;
+
+	/**
+	 * Addon base dir.
+	 *
+	 * @since 1.0
+	 */
+	public string $base_dir;
+
+	/**
+	 * Addon id.
+	 * It's uniq id that we use only for our plugin addons.
+	 *
+	 * @since 1.0
+	 */
+	public string $id;
+
+	/**
+	 * Set addon slug.
+	 *
+	 * @since 1.0
+	 */
+	public function set_addon_slug( string $addon_slug ): Addon {
+		$this->addon_slug = $addon_slug;
+		return $this;
+	}
+
+	/**
+	 * Set addon template.
+	 *
+	 * @since 1.0
+	 */
+	public function set_template( string $template ): Addon {
+		$this->template = $template;
+		return $this;
+	}
+
+	/**
+	 * Set addon config.
+	 *
+	 * @since 1.0
+	 */
+	public function set_config( array $config ): Addon {
+		$this->config = $config;
+		return $this;
+	}
+
+	/**
+	 * Set addon base dir.
+	 *
+	 * @since 1.0
+	 */
+	public function set_addon_base_dir( string $base_dir ): Addon {
+		$this->base_dir = $base_dir;
+		return $this;
+	}
+
+	/**
+	 * Addon initialization.
+	 *
+	 * @since 1.0
+	 */
+	abstract public function init_addon(): void;
+
+	/**
+	 * Render addon.
+	 *
+	 * @since 1.0
+	 */
+	abstract public function render_addon( array $atts ): string;
+
+	/**
+	 * Get path to addon asset.
+	 *
+	 * @since 1.0
+	 */
+	public function get_addon_asset_path( string $file_path ): string {
+		return $this->base_dir . '/' . $file_path;
+	}
+
+	/**
+	 * Enqueue addon dependency assets.
+	 * Assets can be inner and external.
+	 *
+	 * @since 1.0
+	 */
+	public function enqueue_addon_assets(): void {
+		if ( empty( $this->config['assets'] ) ) {
+			return;
+		}
+
+		$this->enqueue_addon_inner_assets( $this->config['assets'] );
+		$this->enqueue_addon_external_assets( $this->config['assets'] );
+	}
+
+	/**
+	 * Enqueue addon dependency inner assets.
+	 *
+	 * @since 1.0
+	 */
+	public function enqueue_addon_inner_assets( array $assets ): void {
+		if ( empty( $assets['inner'] ) ) {
+			return;
+		}
+
+		foreach ( $assets['inner'] as $type => $assets_data ) {
+			if ( empty( $assets_data['file'] ) ) {
+				continue;
+			}
+
+			$this->enqueue_single_inner_asset( $type, $assets_data );
+		}
+	}
+
+	/**
+	 * Enqueue addon dependency external assets.
+	 *
+	 * @since 1.0
+	 */
+	public function enqueue_addon_external_assets( array $assets ): void {
+		if ( empty( $assets['external'] ) ) {
+			return;
+		}
+
+		foreach ( $assets['external'] as $type => $assets_data ) {
+			if ( empty( $assets_data['url'] ) ) {
+				continue;
+			}
+
+			$this->enqueue_single_external_asset( $type, $assets_data );
+		}
+	}
+
+	/**
+	 * Enqueue single external addon asset.
+	 *
+	 * @since 1.0
+	 */
+	public function enqueue_single_external_asset( string $type, array $asset ): void {
+		$options    = $this->extract_asset_options( $asset );
+		$asset_name = $this->external_assets_prefix . '-' . $asset['url'];
+
+		if ( 'js' === $type ) {
+			wp_enqueue_script( $asset_name, $asset['url'], $options['deps'], JOYWPTESTIMONIALSWPB_VERSION, $options['args'] );
+		} elseif ( 'css' === $type ) {
+			wp_enqueue_style( $asset_name, $asset['url'], $options['deps'], JOYWPTESTIMONIALSWPB_VERSION, $options['media'] );
+		}
+	}
+
+	/**
+	 * Enqueue single inner addon asset.
+	 *
+	 * @since 1.0
+	 */
+	public function enqueue_single_inner_asset( string $type, array $asset ): void {
+		$path = $this->get_addon_asset_path( $asset['file'] );
+		if ( ! file_exists( $path ) ) {
+			return;
+		}
+
+		$options = $this->extract_asset_options( $asset );
+		$url     = joywptestimonialswpb_convert_path_to_uri( $path );
+
+		$asset_name = $this->addon_slug . '-' . $asset['file'];
+
+		if ( 'js' === $type ) {
+			wp_enqueue_script( $asset_name, $url, $options['deps'], JOYWPTESTIMONIALSWPB_VERSION, $options['args'] );
+		} elseif ( 'css' === $type ) {
+			wp_enqueue_style( $asset_name, $url, $options['deps'], JOYWPTESTIMONIALSWPB_VERSION, $options['media'] );
+		}
+	}
+
+	/**
+	 * Extract asset enqueue arguments from the shortcode configuration settings
+	 *
+	 * @since 1.0
+	 */
+	private function extract_asset_options( array $asset ): array {
+		return [
+			'deps'  => $asset['deps'] ?? [],
+			'args'  => $asset['args'] ?? [],
+			'media' => $asset['media'] ?? 'all',
+		];
+	}
+}
--- src/Addons/Builders/Wpbakery/Addon/Addon.php
+++ src/Addons/Builders/Wpbakery/Addon/Addon.php
@@ -8,6 +8,7 @@
 namespace JoywpTestimonialsWpb\Addons\Builders\Wpbakery\Addon;
 
 use WPBakeryShortCode;
+use JoywpTestimonialsWpb\Addons\AbstractAddon;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -16,52 +17,16 @@ defined( 'ABSPATH' ) || exit;
  *
  * @since 1.0
  */
-class Addon {
-	/**
-	 * Addon slug.
-	 *
-	 * @since 1.0
-	 */
-	public string $addon_slug;
-
-	/**
-	 * Addon template file.
-	 *
-	 * @since 1.0
-	 */
-	public string $template;
-
+class Addon extends AbstractAddon {
 	/**
 	 * Addon attributes.
 	 *
 	 * Attributes - options values
-	 * that use can pass when edit addon.
+	 * that user can pass when edit addon.
 	 *
 	 * @since 1.0
 	 */
 	public array $addon_atts;
-
-	/**
-	 * Addon config.
-	 *
-	 * @since 1.0
-	 */
-	public array $config;
-
-	/**
-	 * Addon base dir.
-	 *
-	 * @since 1.0
-	 */
-	public string $base_dir;
-
-	/**
-	 * Addon id.
-	 * It's uniq id that we use only for our plugin addons.
-	 *
-	 * @since 1.0
-	 */
-	public string $id;
 
 	/**
 	 * Addon manager class instance.
@@ -76,46 +41,6 @@ class Addon {
 	 * @since 1.0
 	 */
 	public string $external_assets_prefix = 'joywp';
-
-	/**
-	 * Set addon slug.
-	 *
-	 * @since 1.0
-	 */
-	public function set_addon_slug( string $addon_slug ): Addon {
-		$this->addon_slug = $addon_slug;
-		return $this;
-	}
-
-	/**
-	 * Set addon template.
-	 *
-	 * @since 1.0
-	 */
-	public function set_template( string $template ): Addon {
-		$this->template = $template;
-		return $this;
-	}
-
-	/**
-	 * Set addon config.
-	 *
-	 * @since 1.0
-	 */
-	public function set_config( array $config ): Addon {
-		$this->config = $config;
-		return $this;
-	}
-
-	/**
-	 * Set addon base dir.
-	 *
-	 * @since 1.0
-	 */
-	public function set_addon_base_dir( string $base_dir ): Addon {
-		$this->base_dir = $base_dir;
-		return $this;
-	}
 
 	/**
 	 * Set WPBakeryShortCode.
@@ -158,120 +83,6 @@ class Addon {
 		ob_end_clean();
 
 		return $output;
-	}
-
-	/**
-	 * Get path to addon asset.
-	 *
-	 * @since 1.0
-	 */
-	public function get_addon_asset_path( string $file_path ): string {
-		return $this->base_dir . '/' . $file_path;
-	}
-
-	/**
-	 * Enqueue addon dependency assets.
-	 * Assets can be inner and external.
-	 *
-	 * @since 1.0
-	 */
-	public function enqueue_addon_assets(): void {
-		if ( empty( $this->config['assets'] ) ) {
-			return;
-		}
-
-		$this->enqueue_addon_inner_assets( $this->config['assets'] );
-		$this->enqueue_addon_external_assets( $this->config['assets'] );
-	}
-
-	/**
-	 * Enqueue addon dependency inner assets.
-	 *
-	 * @since 1.0
-	 */
-	public function enqueue_addon_inner_assets( array $assets ): void {
-		if ( empty( $assets['inner'] ) ) {
-			return;
-		}
-
-		foreach ( $assets['inner'] as $type => $assets_data ) {
-			if ( empty( $assets_data['file'] ) ) {
-				continue;
-			}
-
-			$this->enqueue_single_inner_asset( $type, $assets_data );
-		}
-	}
-
-	/**
-	 * Enqueue addon dependency external assets.
-	 *
-	 * @since 1.0
-	 */
-	public function enqueue_addon_external_assets( array $assets ): void {
-		if ( empty( $assets['external'] ) ) {
-			return;
-		}
-
-		foreach ( $assets['external'] as $type => $assets_data ) {
-			if ( empty( $assets_data['url'] ) ) {
-				continue;
-			}
-
-			$this->enqueue_single_external_asset( $type, $assets_data );
-		}
-	}
-
-	/**
-	 * Enqueue single external addon asset.
-	 *
-	 * @since 1.0
-	 */
-	public function enqueue_single_external_asset( string $type, array $asset ): void {
-		$options    = $this->extract_asset_options( $asset );
-		$asset_name = $this->external_assets_prefix . '-' . $asset['url'];
-
-		if ( 'js' === $type ) {
-			wp_enqueue_script( $asset_name, $asset['url'], $options['deps'], JOYWPTESTIMONIALSWPB_VERSION, $options['args'] );
-		} elseif ( 'css' === $type ) {
-			wp_enqueue_style( $asset_name, $asset['url'], $options['deps'], JOYWPTESTIMONIALSWPB_VERSION, $options['media'] );
-		}
-	}
-
-	/**
-	 * Enqueue single inner addon asset.
-	 *
-	 * @since 1.0
-	 */
-	public function enqueue_single_inner_asset( string $type, array $asset ): void {
-		$path = $this->get_addon_asset_path( $asset['file'] );
-		if ( ! file_exists( $path ) ) {
-			return;
-		}
-
-		$options = $this->extract_asset_options( $asset );
-		$url     = joywptestimonialswpb_convert_path_to_uri( $path );
-
-		$asset_name = $this->addon_slug . '-' . $asset['file'];
-
-		if ( 'js' === $type ) {
-			wp_enqueue_script( $asset_name, $url, $options['deps'], JOYWPTESTIMONIALSWPB_VERSION, $options['args'] );
-		} elseif ( 'css' === $type ) {
-			wp_enqueue_style( $asset_name, $url, $options['deps'], JOYWPTESTIMONIALSWPB_VERSION, $options['media'] );
-		}
-	}
-
-	/**
-	 * Extract asset enqueue arguments from the shortcode configuration settings
-	 *
-	 * @since 1.0
-	 */
-	private function extract_asset_options( array $asset ): array {
-		return [
-			'deps'  => $asset['deps'] ?? [],
-			'args'  => $asset['args'] ?? [],
-			'media' => $asset['media'] ?? 'all',
-		];
 	}
 
 	/**
@@ -351,18 +162,5 @@ class Addon {
 		}
 
 		return '';
-	}
-
-	/**
-	 * We have a set of libs that help us work with addon attributes.
-	 * To access them we use this method.
-	 *
-	 * @since 1.0
-	 */
-	public function get_atts_lib( string $lib_name ): object {
-		$lib_name  = str_replace( '-', '', ucwords( $lib_name, '-' ) );
-		$lib_class = 'JoywpWpbTimeline\\Shortcodes\\ShortcodeAttsLib\\' . $lib_name;
-
-		return new $lib_class();
 	}
 }
