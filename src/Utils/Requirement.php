4f9f6fdb8039b402c60736f5dfbc6b42bc9802a2
DanilaStrogov <admin@joywp.com>
2026-01-16 11:04:44 +0200

    Add close to requrement notice

--- src/Utils/Requirement.php
+++ src/Utils/Requirement.php
@@ -150,6 +150,6 @@ class Requirement {
 			'<strong>' . $name . '</strong>'
 		);
 		$requirements = count( $this->does_not_meet ) === 0 ? '' : '<ul><li>' . implode( '</li><li>', $this->does_not_meet ) . '</li></ul>';
-		printf( '<div class="notice notice-error"><p>%1$s</p>%2$s</div>', wp_kses_post( $notice ), wp_kses_post( $requirements ) );
+		printf( '<div class="notice notice-error is-dismissible"><p>%1$s</p>%2$s</div>', wp_kses_post( $notice ), wp_kses_post( $requirements ) );
 	}
 }
