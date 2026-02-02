ef192f6cf50263254123cd44988198274acf7ae2
DanilaStrogov <admin@joywp.com>
2026-02-02 11:36:52 +0200

    Add default content for testimonial-quote-with-avatar addon

---
+++ addons/testimonial-quote-with-avatar/defaults.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Default params addon values.
+ *
+ * @since 1.0
+ */
+
+return [
+	'testimonial'            => '<span style="font-size: 12px;"><em>Thank you. before I begin, I\'d like everyone to notice that my report is in a professional, clear plastic binder...When a report looks this good, you know it\'ll get an A. That\'s a tip kids. Write it down.</em></span>',
+	'name'                   => '<div><strong><span style="color: #333333;">Eleanor Faint</span></strong></div><div><span style="font-size: 12px;">LIttleSnippets.net</span></div>',
+	'testimonial_background' => '#FAFAFA',
+	'gap'                    => '50',
+	'add_quotes'             => 'true',
+	'quotes_color'           => '#BEBEBE',
+	'add_image'              => 'true',
+	'source'                 => 'external_link',
+	'external_img_size'      => '90x90',
+	'external_style'         => 'vc_box_circle',
+	'add_pointer'            => 'true',
+	'pointer_color'          => '#FAFAFA',
+	'pointer_position'       => '13',
+	'add_border'             => 'true',
+	'border_width'           => '0',
+	'border_radius'          => '5',
+	'advanced_divider'       => '1',
+	'custom_src'             => 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample17.jpg',
+];
