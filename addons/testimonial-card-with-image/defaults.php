77df2185d3f58f17067bcbd4c495d8ceffec265a
DanilaStrogov <admin@joywp.com>
2026-03-02 17:06:36 +0200

    Fix default values in existing addons

--- addons/testimonial-card-with-image/defaults.php
+++ addons/testimonial-card-with-image/defaults.php
@@ -9,7 +9,7 @@ return [
 	'testimonial'                => '<p><span style="font-size: 12px; color: #333333;"><em>Sometimes I think the surest sign that intelligent life exists elsewhere in the universe is that none of it has tried to contact us.</em></span></p>',
 	'add_quotes'                 => 'true',
 	'main_add_image'             => 'true',
-	'source'                     => 'external_link',
+	'main_image_source'          => 'external_link',
 	'add_name'                   => 'true',
 	'name'                       => '<p><span style="font-family: \'andale mono\', monospace;"><strong>PelicanSteve</strong></span>LittleMan</p>',
 	'add_pointer'                => 'true',
@@ -20,7 +20,7 @@ return [
 	'main_box_shadow_vertical'   => '1',
 	'main_box_shadow_blur'       => '4',
 	'advanced_divider'           => '1',
-	'custom_src'                 => 'https://i.pravatar.cc/500?img=12',
+	'main_image_custom_src'      => 'https://i.pravatar.cc/500?img=12',
 	'quotes_color'               => '#33333333',
 	'main_box_shadow_color'      => '#00000082',
 ];
