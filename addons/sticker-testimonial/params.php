9943c23cf6150bba49e9fdc20b96c3662140d7b0
DanilaStrogov <admin@joywp.com>
2026-03-20 13:01:15 +0200

    feat: #1 add addon template

--- addons/sticker-testimonial/params.php
+++ addons/sticker-testimonial/params.php
@@ -32,6 +32,57 @@ return array_merge(
 				'use_media'   => 'false',
 			],
 		],
+		[
+			'type'        => 'joywp_switcher',
+			'param_name'  => 'add_quotes',
+			'heading'     => esc_html__( 'Add Quotes', 'joywp-testimonials-wpbakery-addons' ),
+			'description' => esc_html__( 'Add quotes before and after testimonial text.', 'joywp-testimonials-wpbakery-addons' ),
+			'options'     => [
+				'true' => [
+					'label' => '',
+					'on'    => __( 'Yes', 'joywp-testimonials-wpbakery-addons' ),
+					'off'   => __( 'No', 'joywp-testimonials-wpbakery-addons' ),
+				],
+			],
+			'value'       => '',
+			'group'       => esc_html__( 'Quotes', 'joywp-testimonials-wpbakery-addons' ),
+		],
+		[
+			'type'        => 'joywp_number',
+			'heading'     => esc_html__( 'Size', 'joywp-testimonials-wpbakery-addons' ),
+			'param_name'  => 'quotes_size',
+			'description' => esc_html__( 'Enter size for quotes.', 'joywp-testimonials-wpbakery-addons' ),
+			'title'       => esc_html__( 'px', 'joywp-testimonials-wpbakery-addons' ),
+			'value'       => '40',
+			'min'         => '10',
+			'max'         => '100',
+			'step'        => '1',
+			'group'       => esc_html__( 'Quotes', 'joywp-testimonials-wpbakery-addons' ),
+			'dependency'  => [
+				'element' => 'add_quotes',
+				'value'   => 'true',
+			],
+		],
+		[
+			'type'        => 'colorpicker',
+			'heading'     => esc_html__( 'Color', 'joywp-testimonials-wpbakery-addons' ),
+			'param_name'  => 'quotes_color',
+			'description' => esc_html__( 'Select color for quotes.', 'joywp-testimonials-wpbakery-addons' ),
+			'group'       => esc_html__( 'Quotes', 'joywp-testimonials-wpbakery-addons' ),
+			'dependency'  => [
+				'element' => 'add_quotes',
+				'value'   => 'true',
+			],
+		],
 	],
-	[]
+	$config
+		->get_collection( 'font-family', 'main' )
+		->set_additional_params( [ 'group' => __( 'Quotes', 'joywp-testimonials-wpbakery-addons' ) ] )
+		->set_dependency(
+			[
+				'element' => 'add_quotes',
+				'value'   => 'true',
+			]
+		)
+		->get_params(),
 );
