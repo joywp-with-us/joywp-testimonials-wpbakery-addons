cb6b1929603c6dd88fe6397d030644a7dce33533
DanilaStrogov <admin@joywp.com>
2026-03-27 15:47:45 +0200

    feat: #1 move border collection output to template

---
+++ templates/collections/border.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Border collection output template
+ *
+ * @since 1.0
+ * @var string $width
+ * @var string $style
+ * @var string $color
+ * @var string $radius
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( $width ) {
+	?>
+	border-width: <?php echo esc_attr( $width ); ?>px;
+	<?php
+}
+if ( $style ) {
+	?>
+	border-style: <?php echo esc_attr( $style ); ?>;
+	<?php
+}
+if ( $color ) {
+	?>
+	border-color: <?php echo esc_attr( $color ); ?>;
+	<?php
+}
+if ( $radius ) {
+	?>
+	border-radius: <?php echo esc_attr( $radius ); ?>px;
+	<?php
+}
