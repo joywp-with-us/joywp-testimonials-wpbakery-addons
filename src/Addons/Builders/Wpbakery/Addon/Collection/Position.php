16d5cd436b7e47a178d42082d12c4c7462fff81d
DanilaStrogov <admin@joywp.com>
2026-03-27 16:14:07 +0200

    feat: #1 move postion collection output to template

--- src/Addons/Builders/Wpbakery/Addon/Collection/Position.php
+++ src/Addons/Builders/Wpbakery/Addon/Collection/Position.php
@@ -26,26 +26,16 @@ class Position extends AbstractAddonCollection {
 	 * @since 1.0
 	 */
 	public function get_render_output( array $atts ): string {
-		$result = '';
-		$unit   = $this->collection->get_unit();
-		$top    = $atts[ $this->collection->get_param_slug( 'top' ) ];
-		if ( $top ) {
-			$result .= 'top: ' . esc_attr( $top ) . esc_attr( $unit ) . '; ';
-		}
-		$bottom = $atts[ $this->collection->get_param_slug( 'bottom' ) ];
-		if ( $bottom ) {
-			$result .= 'bottom: ' . esc_attr( $bottom ) . esc_attr( $unit ) . '; ';
-		}
-		$right = $atts[ $this->collection->get_param_slug( 'right' ) ];
-		if ( $right ) {
-			$result .= 'right: ' . esc_attr( $right ) . esc_attr( $unit ) . '; ';
-		}
-		$left = $atts[ $this->collection->get_param_slug( 'left' ) ];
-		if ( $left ) {
-			$result .= 'left: ' . esc_attr( $left ) . esc_attr( $unit ) . '; ';
-		}
-
-		return $result;
+		return joywptestimonialswpb_get_template(
+			'collections/position.php',
+			[
+				'top'    => $atts[ $this->collection->get_param_slug( 'top' ) ] ?? '',
+				'bottom' => $atts[ $this->collection->get_param_slug( 'bottom' ) ] ?? '',
+				'right'  => $atts[ $this->collection->get_param_slug( 'right' ) ] ?? '',
+				'left'   => $atts[ $this->collection->get_param_slug( 'left' ) ] ?? '',
+				'unit'   => $this->collection->get_unit(),
+			]
+		);
 	}
 
 
---
+++ templates/collections/position.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Position collection output template
+ *
+ * @since 1.0
+ * @var string $top
+ * @var string $bottom
+ * @var string $right
+ * @var string $left
+ * @var string $unit
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( $top ) {
+	?>
+	top: <?php echo esc_attr( $top ) . esc_attr( $unit ); ?>;
+	<?php
+}
+if ( $bottom ) {
+	?>
+	bottom: <?php echo esc_attr( $bottom ) . esc_attr( $unit ); ?>;
+	<?php
+}
+if ( $right ) {
+	?>
+	right: <?php echo esc_attr( $right ) . esc_attr( $unit ); ?>;
+	<?php
+}
+if ( $left ) {
+	?>
+	left: <?php echo esc_attr( $left ) . esc_attr( $unit ); ?>;
+	<?php
+}
