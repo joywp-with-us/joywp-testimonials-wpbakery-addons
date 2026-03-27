a97b26e55e30b54e70666ddb1e8aaa26ed02cde7
DanilaStrogov <admin@joywp.com>
2026-03-27 18:07:46 +0200

    feat: #3 add basic template for joywp_testimonial_profile_card_slider_wrapper addon

---
+++ addons/testimonial-profile-card-slider/params.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Params for joywp_testimonial_profile_card_slider_wrapper addon.
+ *
+ * @var ConfigManager $config
+ * @since 1.0
+ */
+
+use JoywpTestimonialsWpb\Addons\ConfigManager;
+
+defined( 'ABSPATH' ) || exit;
+
+return array_merge(
+	[],
+	[],
+);
---
+++ addons/testimonial-profile-card-slider/template.php
@@ -0,0 +1,360 @@
+<?php
+/**
+ * The template for displaying joywp_testimonial_profile_card_slider_wrapper addon output.
+ *
+ * @since 1.0
+ * @var array $atts
+ * @var JoywpTestimonialsWpb\Addons\AbstractAddon $addon
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
+<div class="joywp-testimonial-profile-card-slider-wrapper">
+    <div class="joywp-testimonial-profile-card-slider-slider">
+        <div class="joywp-testimonial-profile-card-slider__cards">
+            <div class="joywp-testimonial-profile-card-slider">
+                <div class="joywp-testimonial-profile-card-slider__content">
+                    <div class="joywp-testimonial-profile-card-slider__text">
+                        Zen Doan is a business analyst, entrepreneur and media proprietor, and investor. She also known as the
+                        best selling book author.
+                    </div>
+                    <div class="joywp-testimonial-profile-card-slider__author">
+                        <div class="joywp-testimonial-profile-card-slider__name">Zen</div>
+                        <div class="joywp-testimonial-profile-card-slider__role">Author</div>
+                    </div>
+                </div>
+                <div class="joywp-testimonial-profile-card-slider__hero">
+                    <img
+                            src="https://user-images.githubusercontent.com/13468728/234031693-6bbaba7d-632c-4d7d-965f-75a76a549ce2.jpg"
+                            alt="avatar">
+                </div>
+            </div>
+
+            <div class="joywp-testimonial-profile-card-slider">
+                <div class="joywp-testimonial-profile-card-slider__content">
+                    <div class="joywp-testimonial-profile-card-slider__text">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores veniam maiores quaerat ut earum
+                        molestiae
+                        commodi, repellat voluptas ad eligendi eveniet doloremque aliquam tempora consectetur eaque. Repellat
+                        ratione iste
+                        dicta!
+                        Sapiente, soluta id tempora sunt vel cumque quis repellendus dolore. Possimus animi consequatur voluptate
+                        expedita ex
+                        magni eos doloremque labore sed repellat. Quos soluta facere inventore doloribus nostrum asperiores
+                        provident!
+                        Adipisci, enim quibusdam! Dolores, minus reprehenderit tempora ratione quia quam natus vel earum delectus
+                        voluptatibus
+                        eius soluta praesentium facere, exercitationem cupiditate qui accusamus iste! Fugiat neque deleniti
+                        pariatur minima
+                        aspernatur!
+                        Nostrum fuga iusto voluptate aliquid, deserunt possimus! Magnam laudantium molestiae exercitationem neque
+                        veniam,
+                        placeat obcaecati cum, dolore architecto officiis culpa ipsum eius iusto adipisci dignissimos similique
+                        quasi, beatae
+                        maiores! Eos.
+                        Tempora laudantium dolores sunt fugit dignissimos in enim nam ullam animi magnam molestiae aperiam tempore
+                        modi
+                        perferendis similique amet qui, delectus, repellat et perspiciatis dolor tenetur hic maxime libero. Iste.
+                        Aspernatur, illum. Dignissimos eligendi facilis quibusdam voluptas perspiciatis, quaerat odio pariatur,
+                        excepturi
+                        quasi nemo reiciendis possimus molestiae maxime quas amet maiores aperiam est commodi culpa? Nesciunt, in.
+                        Ipsam,
+                        optio assumenda!
+                        Ratione sed ex dolor natus fuga ullam corrupti! Impedit harum temporibus similique dolor distinctio ipsam
+                        deserunt,
+                        necessitatibus voluptates perspiciatis? Consectetur tenetur, eveniet eligendi ex odio pariatur recusandae
+                        iure minus
+                        impedit.
+                        Quis doloremque officiis omnis recusandae sed earum rerum autem! Enim vel nisi sunt aliquam sequi illo
+                        quis quas
+                        inventore excepturi vero eos aspernatur, deleniti voluptates nulla quo sed ex. Aliquam.
+                        Libero quisquam repellat, officiis aspernatur accusamus quos pariatur! Nesciunt nihil quis similique?
+                        Provident vel
+                        fugiat assumenda deserunt minima nulla? Ab, atque! Natus et amet esse? Est distinctio dicta quo suscipit.
+                        Sit odio, enim deleniti eum dicta voluptatibus veniam quas error unde quaerat cumque quisquam, facilis
+                        temporibus.
+                        Numquam reiciendis soluta ex repellat. Quaerat sunt atque nesciunt et tempora tempore, sequi voluptatibus?
+                    </div>
+                    <div class="joywp-testimonial-profile-card-slider__author">
+                        <span class="joywp-testimonial-profile-card-slider__name">Jonathan</span>
+                        <span class="joywp-testimonial-profile-card-slider__role">Treymont Inc.</span>
+                    </div>
+                </div>
+                <div class="joywp-testimonial-profile-card-slider__hero">
+                    <img
+                            src="https://user-images.githubusercontent.com/13468728/234031617-2dfb19ea-01d0-4370-b63b-bb6bdfb4f78e.jpg"
+                            alt="avatar">
+                </div>
+            </div>
+
+            <div class="joywp-testimonial-profile-card-slider">
+                <div class="joywp-testimonial-profile-card-slider__content">
+                    <div class="joywp-testimonial-profile-card-slider__text">
+                        Charlie Green is an European entrepreneur and media consultant, and investor. He is the founder of the
+                        Hallmark Inc.
+                    </div>
+                    <div class="joywp-testimonial-profile-card-slider__author">
+                        <div class="joywp-testimonial-profile-card-slider__name">Charlie</div>
+                        <div class="joywp-testimonial-profile-card-slider__role">Hallmark Inc.</div>
+                    </div>
+                </div>
+                <div class="joywp-testimonial-profile-card-slider__hero">
+                    <img
+                            src="https://user-images.githubusercontent.com/13468728/234031646-10533999-39e5-4c7b-ab54-d0299b13ce74.jpg"
+                            alt="avatar">
+                </div>
+            </div>
+
+            <div class="joywp-testimonial-profile-card-slider">
+                <div class="joywp-testimonial-profile-card-slider__content">
+                    <div class="joywp-testimonial-profile-card-slider__text">
+                        Sarah Dam is an American internet entrepreneur and media proprietor, and investor. She is the founder of
+                        the multi-national technology company Zara.
+                    </div>
+                    <div class="joywp-testimonial-profile-card-slider__author">
+                        <div class="joywp-testimonial-profile-card-slider__name">Sarah</div>
+                        <div class="joywp-testimonial-profile-card-slider__role">Zara Inc.</div>
+                    </div>
+                </div>
+                <div class="joywp-testimonial-profile-card-slider__hero">
+                    <img src="https://github.com/ecemgo/ecemgo/assets/13468728/55116c98-5f9a-4b0a-9fdb-4911b52d5ef3"
+                         alt="avatar">
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="joywp-testimonial-profile-card-slider__navigation">
+        <div class="joywp-testimonial-profile-card-slider__navigation-button active"></div>
+        <div class="joywp-testimonial-profile-card-slider__navigation-button"></div>
+        <div class="joywp-testimonial-profile-card-slider__navigation-button"></div>
+        <div class="joywp-testimonial-profile-card-slider__navigation-button"></div>
+    </div>
+</div>
+
+<style>
+    .joywp-testimonial-profile-card-slider-wrapper * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper {
+        width: 100%;
+        font-family: 'Poppins', sans-serif;
+        container-type: inline-size;
+
+        background-image: radial-gradient(at 40% 20%,
+        rgb(255, 184, 122) 0px,
+        transparent 50%),
+        radial-gradient(at 80% 0%, rgb(31, 221, 255) 0px, transparent 50%),
+        radial-gradient(at 0% 50%, rgb(255, 219, 222) 0px, transparent 50%),
+        radial-gradient(at 80% 50%, rgb(255, 133, 173) 0px, transparent 50%),
+        radial-gradient(at 0% 100%, rgb(255, 181, 138) 0px, transparent 50%),
+        radial-gradient(at 80% 100%, rgb(107, 102, 255) 0px, transparent 50%),
+        radial-gradient(at 0% 0%, rgb(255, 133, 167) 0px, transparent 50%);
+        background-repeat: no-repeat;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider-slider {
+        width: 100%;
+        overflow: hidden;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__cards {
+        width: 100%;
+        display: flex;
+        transition: 0.5s;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider {
+        position: relative;
+        flex: 0 0 100%;
+        width: 100%;
+        min-height: 400px;
+        overflow: hidden;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__hero {
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 100%;
+        width: 40%;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        border-radius: 10px;
+        overflow: hidden;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__hero img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+        pointer-events: none;
+        user-select: none;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__content {
+        position: absolute;
+        left: 0;
+        top: 50%;
+        transform: translateY(-50%);
+
+        width: 70%;
+        min-height: 270px;
+        max-height: 100%;
+
+        color: #4d4352;
+        background: rgba(255, 255, 255, 0.7);
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(4.5px);
+        -webkit-backdrop-filter: blur(4.5px);
+        border-radius: 10px;
+        padding: 45px;
+        z-index: 2;
+        user-select: none;
+
+        overflow-x: hidden;
+        overflow-y: auto;
+
+        scrollbar-width: thin;
+        scrollbar-color: #999 transparent;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__content::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__content::-webkit-scrollbar-track {
+        background: transparent;
+        border-radius: 8px;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__content::-webkit-scrollbar-thumb {
+        background: #999;
+        border-radius: 8px;
+        transition: 0.3s;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__content::-webkit-scrollbar-thumb:hover {
+        background: linear-gradient(180deg, #aaa, #666);
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__text {
+        font-size: 1.25rem;
+        font-weight: 400;
+        line-height: 1.3;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__author {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__name {
+        font-size: 1.5rem;
+        font-weight: 600;
+        margin-top: 35px;
+        color: #4d4352;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__navigation {
+        display: flex;
+        justify-content: center;
+        margin-top: 4rem;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__navigation-button {
+        display: inline-block;
+        height: 15px;
+        width: 15px;
+        margin: 4px;
+        border-radius: 15px;
+        background: #fff;
+        cursor: pointer;
+        transition: all 0.5s ease-in-out;
+    }
+
+    .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__navigation-button.active {
+        width: 30px;
+    }
+
+    @container (max-width: 500px) {
+        .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__hero {
+            position: static;
+            width: 100%;
+            min-width: 0px;
+            flex: 0 0 400px;
+            margin-bottom: 300px;
+        }
+
+        .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__content {
+            top: 300px;
+            transform: none;
+
+            width: 100%;
+            max-height: 400px;
+            padding: 15px;
+        }
+
+        .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__text {
+            font-size: 0.85em;
+        }
+
+        .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__name {
+            font-size: 0.9em;
+        }
+
+        .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__role {
+            font-size: 0.75em;
+        }
+
+        .joywp-testimonial-profile-card-slider-wrapper .joywp-testimonial-profile-card-slider__navigation {
+            margin-top: 30px;
+        }
+    }
+</style>
+
+<script>
+    const sliders = document.querySelectorAll('.joywp-testimonial-profile-card-slider-slider')
+
+    sliders.forEach(slider => {
+        const sliderCards = document.querySelector(".joywp-testimonial-profile-card-slider__cards")
+        const navigationButtons = document.querySelectorAll(".joywp-testimonial-profile-card-slider__navigation-button")
+
+        let currentIndex = 0
+
+        function updateSlide() {
+            const sliderWidth = slider.getBoundingClientRect().width
+            const translatedDistance = sliderWidth * currentIndex
+            sliderCards.style.transform = `translateX(-${translatedDistance}px)`
+
+            navigationButtons.forEach((btn, index) => {
+                btn.classList.toggle("active", index === currentIndex)
+            })
+        }
+
+        navigationButtons.forEach((btn, index) => {
+            btn.addEventListener("click", () => {
+                currentIndex = index
+                updateSlide()
+            })
+        })
+
+        window.addEventListener("resize", () => {
+            updateSlide()
+        })
+    })
+</script>
