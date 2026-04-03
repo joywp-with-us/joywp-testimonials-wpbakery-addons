8f088f77018c5cf756fce4cf54d5e3e09dd54418
DanilaStrogov <admin@joywp.com>
2026-04-03 12:13:10 +0300

    feat: #6 add localization file

--- languages/json-localizer.php
+++ languages/json-localizer.php
@@ -2,7 +2,11 @@
 // This file is auto-generated for json translation only
 __( 'Interactive Shuffling Testimonials', 'joywp-testimonials-wpbakery-addons' );
 __( 'Interactive shuffling testimonials module.', 'joywp-testimonials-wpbakery-addons' );
+__( 'Sticker Testimonial', 'joywp-testimonials-wpbakery-addons' );
+__( 'Testimonial with sticker style addon.', 'joywp-testimonials-wpbakery-addons' );
 __( 'Testimonial Card With Image', 'joywp-testimonials-wpbakery-addons' );
-__( 'Testimonial Card With Image', 'joywp-testimonials-wpbakery-addons' );
+__( 'Testimonial card with image addon', 'joywp-testimonials-wpbakery-addons' );
+__( 'Testimonial Profile Card Slider', 'joywp-testimonials-wpbakery-addons' );
+__( 'Testimonial profile card slider addon.', 'joywp-testimonials-wpbakery-addons' );
 __( 'Testimonial Quote With Avatar', 'joywp-testimonials-wpbakery-addons' );
-__( 'Testimonial Quote With Avatar', 'joywp-testimonials-wpbakery-addons' );
+__( 'Testimonial quote with avatar addon.', 'joywp-testimonials-wpbakery-addons' );
